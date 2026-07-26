d3a02da2de6592b39af7cb89db0fbfaadf2990e5
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-07-26 11:54:16 +0530

    feat: generate payments dataset

--- database/payments.sql
+++ database/payments.sql
@@ -1,6 +1,12 @@
 DESCRIBE payments;
+-- 
 SELECT COUNT(*) 
 FROM payments;
+-- 
 SELECT payment_method, count(*)
 FROM payments
 GROUP BY payment_method;
+--
+SELECT payment_status, count(*)
+FROM payments
+GROUP BY payment_status; 
