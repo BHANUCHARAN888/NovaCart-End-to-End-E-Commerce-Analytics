10c2f0aab8f395efe2e75facd8f43c47a76cdb55
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-07-26 11:29:12 +0530

    feat: generate customers, products, orders, order_items datasets

--- database/order_items.sql
+++ database/order_items.sql
@@ -6,3 +6,7 @@ DELETE FROM order_items;
 ALTER TABLE order_items AUTO_INCREMENT = 1;
 select count(*) from order_items;
 -- ============================== Checking changes
+-- Average of order items from orders
+SELECT
+    ROUND(COUNT(*) / COUNT(DISTINCT order_id), 2) AS avg_items_per_order
+FROM order_items;
