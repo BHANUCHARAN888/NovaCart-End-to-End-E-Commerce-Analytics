dcc7ca75b78fc8f7bda4804661537305320f11af
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-07-28 23:36:15 +0530

    Analyse the highest and lowest Revenue from monthly Sales

--- solutions/01_sales_analysis.sql
+++ solutions/01_sales_analysis.sql
@@ -35,3 +35,26 @@ inner join payments p
 on o.order_id = p.order_id
 where payment_status = "Success"
 group by date_format(o.order_date,'%Y-%m');
+-- Business Q2: Which months generate the highest and lowest sales?
+select date_format(o.order_date,"%Y-%m") as month,
+sum(oi.quantity*oi.unit_price) as Revenue
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id
+inner join payments p
+on o.order_id = p.order_id
+where payment_status = "Success"
+group by date_format(o.order_date,"%Y-%m")
+order by Revenue desc
+limit 1 ;
+select date_format(o.order_date,"%Y-%m") as month,
+sum(oi.quantity*oi.unit_price) as Revenue
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id
+inner join payments p
+on o.order_id = p.order_id
+where payment_status = "Success"
+group by date_format(o.order_date,"%Y-%m")
+order by Revenue asc
+limit 1 ;
