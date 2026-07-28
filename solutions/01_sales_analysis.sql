5b9d832dd53843d9e19583f6f4ca1c8453600eab
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-07-28 22:21:54 +0530

    Completed monthly revenue trend analysis

---
+++ solutions/01_sales_analysis.sql
@@ -0,0 +1,37 @@
+use novacart;
+select * from products;
+select * from customers;
+select * from payments;
+select * from orders;
+select * from order_items;
+-- Business Q1: How does monthly revenue change throughout the year?
+-- Query for orders of each individual order_items relation.
+select * 
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id;
+-- Combining tables which are required
+select * 
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id
+inner join payments p
+on o.order_id = p.order_id;
+-- filtering the successful payments
+select * 
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id
+inner join payments p
+on o.order_id = p.order_id
+where payment_status='Success';
+-- now getting the month year not complete columns
+select date_format(o.order_date,'%Y-%m') as month,
+sum(oi.quantity*oi.unit_price) as Revenue
+from orders o
+inner join order_items oi
+on o.order_id = oi.order_id
+inner join payments p
+on o.order_id = p.order_id
+where payment_status = "Success"
+group by date_format(o.order_date,'%Y-%m');
